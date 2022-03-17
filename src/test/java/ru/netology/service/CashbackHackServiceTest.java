d258ca841b42e89751a3e15828135e37383ff61b
Sergey Scherbakov <sherbakov_s_a@mail.ru>
2022-03-17 21:49:32 +0300

    completed task 2 JUnit5 и Legacy

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,12 +1,14 @@
 package ru.netology.service;
 
+import static org.junit.Assert.*;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public class CashbackHackServiceTest {
 
     @Test
-    public void shouldRemain1000() {
+    public void shouldRemain1000JUnit4() {
         CashbackHackService service = new CashbackHackService();
         int amount = 0;
 
@@ -17,7 +19,18 @@ public class CashbackHackServiceTest {
     }
 
     @Test
-    public void shouldRemain1() {
+    public void shouldRemain1000JUnitJupiter() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldRemain1JUnit4() {
         CashbackHackService service = new CashbackHackService();
         int amount = 999;
 
@@ -28,7 +41,18 @@ public class CashbackHackServiceTest {
     }
 
     @Test
-    public void shouldRemain0() {
+    public void shouldRemain1JUnitJupiter() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+
+        int actual = service.remain(amount);
+        int expected = 1;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldRemain0JUnit4() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
 
@@ -39,7 +63,18 @@ public class CashbackHackServiceTest {
     }
 
     @Test
-    public void shouldRemain999() {
+    public void shouldRemain0JUnitJupiter() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldRemain999JUnit4() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1001;
 
@@ -47,5 +82,16 @@ public class CashbackHackServiceTest {
         int expected = 999;
 
         Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldRemain999JUnitJupiter() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1001;
+
+        int actual = service.remain(amount);
+        int expected = 999;
+
+        assertEquals(expected, actual);
     }
 }
